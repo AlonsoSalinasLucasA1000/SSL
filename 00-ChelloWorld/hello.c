10750fd2342dece0e32086dcf0389e83ad3b9160
Lucas Alonso <164697469+AlonsoSalinasLucasA1000@users.noreply.github.com>
2024-05-19 03:31:09 +0000

    Añadir archivos finales

---
+++ 00-ChelloWorld/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    //Programa de prueba
+    printf("Hello World\n");
+    printf("Sumar dos números enteros\n");
+    int a;
+    int b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int c = a+b;
+    printf("El valor obtenido es %d",c);
+
+    return 0;
+}
